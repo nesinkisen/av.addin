20bb1e82e829ca72f43e35ae1abc3b27445e8326
nesinkisen <mertgunturkundur@hotmail.com>
2023-08-27 01:01:50 +0300

    maybe

---
+++ public/business.js
@@ -0,0 +1,231 @@
+let platformClient;
+
+var lifecycleStatusMessageTitle = 'Lifecycle Demo App';
+var lifecycleStatusMessageId = 'lifecycleDemo-statusMsg';
+var blurCount = 0;
+var userApiInstance;
+var presenceApiInstance;
+var conversationsApi;
+var analyticsApi;
+var routingApi;
+var notificationsApi;
+var outboundApi;
+var userId;
+// Local vars
+var presences = {};
+var userPresenceTopic = '';
+var userPresenceTopicArr = [];
+var webSocket = null;
+var me, notificationChannel, conversationsTopic;
+var CONVERSATION_LIST_TEMPLATE = null;
+var conversationList = {};     
+var customerIdKeepFor;
+var phoneNumberKeepFor;
+var scheduledTimeKeepFor;
+var agentNameKeepFor;
+var callbackOrNot = false;
+var users = {};
+var statusList = [];
+//npm install -g local-ssl-proxy
+// local-ssl-proxy --source 6500 --target 5500
+const REDIRECT_URI = "https://nesinkisen.github.io/av.addin";
+//const REDIRECT_URI = "http://localhost:5500";  
+var client;
+
+$(function() 
+{
+    platformClient = require('platformClient');
+
+    client = platformClient.ApiClient.instance;
+    
+    //client.setEnvironment(platformClient.PureCloudRegionHosts.us_east_1); // Genesys Cloud region
+    client.setEnvironment(platformClient.PureCloudRegionHosts.eu_west_1);
+
+    // Create API instance
+    //const usersApi = new platformClient.UsersApi();
+    userId = "d2dab52a-8588-4942-8d44-fd8a2aa5d4a2"; // String | user Id    
+    
+    userApiInstance = new platformClient.UsersApi();
+    presenceApiInstance = new platformClient.PresenceApi();
+    conversationsApi = new platformClient.ConversationsApi();
+    analyticsApi = new platformClient.AnalyticsApi(); 
+    routingApi = new platformClient.RoutingApi();       
+    notificationsApi = new platformClient.NotificationsApi();
+    outboundApi = new platformClient.OutboundApi(); 
+
+    // Authenticate
+    client
+    .loginImplicitGrant("502e4415-5faf-4c5e-ae4e-8c46ba24a4a2", REDIRECT_URI)
+    .then(getUserList)
+    .then(getStatusList).then(afterStatusList)
+    .then(getUsersStatusList)
+    .then(getUsersConversationList)
+    .then(getUsersMe).then(afterUsersMe)
+    .then(getChannels).then(afterChannels)
+    .then(onStart)
+    .then(hideOfflineUsers)
+    .catch(handleErrors);
+});
+// Handle incoming Genesys Cloud notification from WebSocket
+function handleNotification(message) 
+{
+// Parse notification string to a JSON object
+    const notification = JSON.parse(message.data);
+
+    var str = notification.topicName, //bu bolum topic url'inden userid ayiklayabilmek icin. gelmiyor cunku notification icinden.
+    delimiter = '.',
+    start = 2,
+    end = 3,
+    tokens = str.split(delimiter).slice(start, end),
+    userId = tokens.join(delimiter);
+
+    if(typeof notification.eventBody.presenceDefinition !== 'undefined')
+    {            
+        var user = users[userId]; //status degisikliginin ana users objesine eklenebilmesi icin. id ile obje icinden cekip, degisiklikleri ekliyoruz
+        var oldStatus = user.status.systemPresence;
+        user.status = 
+        {
+            id: notification.eventBody.presenceDefinition.id,
+            systemPresence: findExactStatusinList(notification.eventBody.presenceDefinition.id),
+            modifiedDate: notification.eventBody.modifiedDate
+        };
+
+        if (oldStatus == "Offline" && user.status.systemPresence != "Offline") //hideoffline checkbox secili iken offline olan biri available busy biseye gecerse table'a eklenebilsin diye refresh.
+        {
+            if ($('#chkHideOfflineUsers').is(':checked')) //ama hideoffline secili degil, yani butun agentlar gorunuyorsa, bisey yapma
+            {
+                AddAllItemsToTable();
+                hideOfflineUsers();
+            }
+        }
+        else if (oldStatus != "Offline" && user.status.systemPresence == "Offline")
+        {
+            if ($('#chkHideOfflineUsers').is(':checked'))
+                hideOfflineUsers();
+        }
+
+        setStatusViaSocket(userId, notification.eventBody.presenceDefinition.id, notification.eventBody.modifiedDate)
+        document.getElementById(userId + "_td2").setAttribute("style", "background-color:" + findStatusBackgroundColor(findExactStatusinList(notification.eventBody.presenceDefinition.id)));       
+    }
+    else
+    {
+        if(typeof notification.eventBody.participants[1].calls[0].connectedTime !== 'undefined')
+        {
+            setInteractionViaSocket(userId, notification.eventBody.participants[1].calls[0].state,
+                                    notification.eventBody.participants[1].calls[0].connectedTime)
+        }
+        else
+            setInteractionViaSocket(userId, notification.eventBody.participants[1].calls[0].state);
+    }
+}
+
+// Determines if a conversation is disconnected by checking to see if all participants are disconnected
+function isConversationDisconnected(conversation)
+{
+	let isConnected = false;
+	conversation.participants.some((participant) =>
+    {
+		if (participant.state !== 'disconnected')
+        {
+			isConnected = true;
+			return true;
+		}
+	});
+
+	return !isConnected;
+}
+
+function getStatusList()
+{
+    // Get presences
+    return presenceApiInstance.getPresencedefinitions({ pageSize: 100 });
+}
+
+function afterStatusList(presenceListing)
+{
+    //console.log(`Found ${presenceListing.entities.length} presences`);
+    statusList = presenceListing.entities;
+
+    var element = {};
+    // Create button for each presence
+    /*
+    presenceListing.entities.forEach((presence) => {
+        //presences[presence.id] = presence;
+        element.id = presence.id;
+        element.statusName = presence.systemPresence;
+        statusList.push(element);
+
+        $('div#presenceButtons').append($('<button>')
+            .addClass('btn btn-primary')
+            .click(() => setPresence(presence.id))
+            .text(presence.languageLabels.en_US)
+        );
+        
+    });*/
+}
+
+function getUsersMe()
+{
+    // Get authenticated user's data, including current presence
+    return userApiInstance.getUsersMe({ expand: ['presence'] });
+}
+
+function afterUsersMe(userMe)
+{
+    me = userMe;
+    // Set current presence text in UI
+    //$('#currentPresence').text(presences[me.presence.presenceDefinition.id].languageLabels.en_US);
+}
+
+async function getChannels()
+{
+    // Create notification channel
+    
+    const channel = await notificationsApi.postNotificationsChannels();
+    console.log("channel: ", channel);
+
+    return channel;
+}
+
+async function afterChannels(channel)
+{
+    console.log('channelStatus: ', channel);
+    notificationChannel = channel;
+
+    // Set up web socket
+    webSocket = new WebSocket(notificationChannel.connectUri);
+    webSocket.onmessage = handleNotification;
+
+    // Subscribe to authenticated user's presence
+    userPresenceTopic = `v2.users.${me.id}.presence`;
+    //userPresenceTopicArr = ["v2.users.${me.id}.presence", "v2.users.30a95f8c-d73e-463f-a72c-bff2c10925bc.presence"];
+    
+    Object.keys(users).forEach(id => 
+    {
+        userPresenceTopicArr.push(`v2.users.${id}.presence`);
+        userPresenceTopicArr.push(`v2.users.${id}.conversations`);
+    })
+
+    const body = userPresenceTopicArr;
+    /*const body = [userPresenceTopic, `v2.users.30a95f8c-d73e-463f-a72c-bff2c10925bc.presence`,
+    `v2.users.d2dab52a-8588-4942-8d44-fd8a2aa5d4a2.conversations`];*/
+    
+    const putChannel = await notificationsApi.putNotificationsChannelSubscriptions(notificationChannel.id, body);
+    console.log("putChannel", putChannel);
+
+    return putChannel;
+}
+
+function onStart()
+{
+    AddAllItemsToTable();
+
+	var div = document.getElementById('loaderContainer');
+    div.style.visibility = 'hidden';    
+}
+
+function handleErrors(err)
+{
+    // Handle failure response
+    console.error(err);
+}
---
+++ public/ui.js
@@ -0,0 +1,201 @@
+//const { ScriptsApi } = require("purecloud-platform-client-v2");
+var tbody;
+
+$(function() 
+{
+    var dateNow = new Date();
+    $('#datetimepicker1').datetimepicker({
+        format: 'YYYY-MM-DD HH:mm',
+        defaultDate: dateNow
+    });
+
+    $("#chkHideOfflineUsers").change(function() 
+    {
+        if(this.checked) 
+        {
+            hideOfflineUsers();
+        }
+        else
+        {
+            AddAllItemsToTable();
+        }
+        
+    });     
+
+    tbody = document.getElementById('tbody1');
+
+    //$("#btnGet").on("click", GetCallbacks);
+
+    /*
+    let opts = { 
+        "expand": ["expand_example"], // [String] | Which fields, if any, to expand.
+        "integrationPresenceSource": "integrationPresenceSource_example" // String | Get your presence for a given integration. This parameter will only be used when presence is provided as an expand.
+    };
+    */           
+
+    loadFrame();
+
+    startTimers();
+});
+
+function loadFrame()
+{
+    if (document.getElementById(REDIRECT_URI))
+    {
+        var frameWindow = document.getElementById(REDIRECT_URI).contentWindow;
+
+        frameWindow.addEventListener("load", function()
+        {
+            var doc = iframe.contentDocument || iframe.contentWindow.document;
+            var target = doc.getElementById("https://apps.mypurecloud.ie");
+
+            target.innerHTML = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+            console.warn(target.innerHTML);
+        });
+    }
+}
+
+function startTimers()
+{
+    var intervalId = window.setInterval(function()
+    {
+        var gridData = document.getElementById('gridData');
+        for (var i = 1, row; row = gridData.rows[i]; i++) 
+        {
+            gridData.rows[i].cells[3].innerHTML = 
+                getTimeDiffinHHmmss(document.getElementById(gridData.rows[i].cells[0].innerHTML + "_modifiedDate").getAttribute("name"));
+
+            if(gridData.rows[i].cells[2].innerHTML !=="")
+            {
+                if(gridData.rows[i].cells[2].innerHTML.includes("disconnected") || gridData.rows[i].cells[2].innerHTML.includes("terminated"))
+                {
+                    document.getElementById(users[userId].name + "_state").innerHTML = "";
+                    setStatus(userId, "6a3af858-942f-489d-9700-5f9bcdcdae9b", startedDate);
+                }                
+                else if(gridData.rows[i].cells[2].innerHTML.includes("connected"))
+                {
+                    gridData.rows[i].cells[2].innerHTML = 
+                                            document.getElementById(gridData.rows[i].cells[0].innerHTML + "_state").getAttribute("for") + ": " +
+                        getTimeDiffinHHmmss(document.getElementById(gridData.rows[i].cells[0].innerHTML + "_state").getAttribute("name"));
+                }              
+            }          
+        }                              
+
+    }, 1000);
+}
+
+function AddItemToTable(userId, agent, statusId, modifiedDate, conversationState, startedDate)
+{
+    var diff_status_time_parsed = getTimeDiffinHHmmss(modifiedDate);
+    var diff_interaction_time_parsed;
+    if(startedDate !== "")
+        diff_interaction_time_parsed = conversationState + ": " + getTimeDiffinHHmmss(startedDate);                
+    else
+        diff_interaction_time_parsed = "";
+
+    let trow = document.createElement("tr");
+    trow.setAttribute("id", userId + "_row");
+
+    let td1 = document.createElement("td");
+    let td2 = document.createElement("td");
+    let td3 = document.createElement("td");
+    let td4 = document.createElement("td");
+
+    td1.innerHTML = agent;
+
+    td3.innerHTML = diff_interaction_time_parsed;
+    td3.setAttribute("style", "font-size:16px;");
+    td3.setAttribute("id", agent + "_state");
+    td3.setAttribute("for", conversationState);
+    td3.setAttribute("name", startedDate);
+    
+    td4.innerHTML = diff_status_time_parsed;
+    td4.setAttribute("id", agent + "_modifiedDate");
+    td4.setAttribute("name", modifiedDate);
+
+    //Create and append select list
+    var selectList = document.createElement("select");
+    selectList.setAttribute("id", userId);
+    selectList.setAttribute("name", agent);
+    //selectList.id = "statusSelect";
+    td2.appendChild(selectList);
+    td2.setAttribute("id", userId + "_td2");
+
+    var option;
+
+    //Create and append the options
+    for (var i = 0; i < statusList.length; i++) 
+    {
+        option = document.createElement("option")    
+        option.value = statusList[i].id;
+        option.text = statusList[i].languageLabels.en_US;
+        selectList.appendChild(option);
+    }                
+    selectList.value = statusId; // combo icinden seciyor
+    var statusColor = findStatusBackgroundColor(findExactStatusinList(statusId));
+    td2.setAttribute("style", "background-color:" + statusColor)
+
+    selectList.onchange = function () 
+    {
+        setStatus(selectList.getAttribute("id"), selectList.value)
+        var statusColor = findStatusBackgroundColor(findExactStatusinList(selectList.value));
+        td2.setAttribute("style", "background-color:" + statusColor)        
+        //console.log(selectList.value);
+        //console.log("status............ + " + selectList.getAttribute("name") + " .. " + selectList.getAttribute("id"));
+    };    
+
+    trow.appendChild(td1);
+    trow.appendChild(td2);
+    trow.appendChild(td3);
+    trow.appendChild(td4);
+
+    tbody.appendChild(trow);
+}
+
+function AddAllItemsToTable()
+{
+    tbody.innerHTML = "";
+    Object.keys(users).forEach(key => 
+    {
+        AddItemToTable(users[key].id, users[key].name, users[key].status.id, users[key].status.modifiedDate,
+                        (typeof users[key].conversation === 'undefined') ? "" : users[key].conversation.state,
+                        (typeof users[key].conversation === 'undefined') ? "" : users[key].conversation.startedDate);
+    })
+}
+function addHours(date, hours) 
+{
+    date.setTime(date.getTime() + hours * 60 * 60 * 1000);
+    return date;
+}
+
+function findStatusBackgroundColor(statusName)
+{
+    if(statusName == "Available")
+        return "green";
+    else if(statusName == "Busy")
+        return "red";
+    else if(statusName == "One-on-One" || statusName == "Staff Meeting" || statusName == "Preparing")
+        return "orange";    
+    else if(statusName == "On Call")
+        return "#A000D8";    
+    else if(statusName == "Away")
+        return "#0233BD";  
+    else if(statusName == "Idle")
+        return "#E7E001";
+}
+
+function hideOfflineUsers()
+{
+    var table = document.getElementById("gridData");
+    for (var i = 1, row; row = table.rows[i]; i++) 
+    {
+       for (var j = 0, col; col = row.cells[j]; j++) 
+       {
+        if(j == 1 && $("#" + row.cells[j].id + " :selected").text() == "Offline")
+        {
+            table.deleteRow(i);
+            i = 0;
+        }
+       }  
+    } 
+}
