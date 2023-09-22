df117bf0af0cfcae6d8ed144685828e8bb198d6b
nesinkisen <mertgunturkundur@hotmail.com>
2023-09-22 16:02:43 +0300

    Update then.js

--- then.js
+++ then.js
@@ -20,7 +20,7 @@ function getUserList()
                 obj[currentValue.id] = currentValue;
                 return obj;
             }, {});
-            console.log(users);
+            console.log("forHim " + users);
             //console.log(`getUsers success! data: ${JSON.stringify(data, null, 2)}`);
         })
         .catch(err =>
